27d20f47d7058cbac38aabe74a464187cbc4d009
ezntek <easontekreviews@icloud.com>
2023-01-01 16:57:41 +0800

    Add a bit of logic, add sample JSONs, fix UI Typos

--- SM64LinuxLauncher-qt/launcher/dialogs.py
+++ SM64LinuxLauncher-qt/launcher/dialogs.py
@@ -1,5 +1,19 @@
 from PyQt5 import QtWidgets
 from uic.build_new_ui import Ui_BuildNewDialog
+from uic.select_baserom_ui import Ui_BaseromSelectDialog
+
+class BaseromSelectDialog(QtWidgets.QDialog):
+    def __init__(self, parent: QtWidgets.QWidget | None = None) -> None:
+        super().__init__(parent)
+        self.ui = Ui_BaseromSelectDialog()
+        self.ui.setupUi(self)
+    
+    def b_continue(self):
+        pass
+
+    def select_baserom(self):
+        pass
+
 
 class BuildNewDialog(QtWidgets.QDialog):
     def __init__(self, repo: dict, parent: QtWidgets.QWidget | None = None) -> None:
@@ -9,12 +23,35 @@ class BuildNewDialog(QtWidgets.QDialog):
         self.repo = repo
         self.ui.l_build_what.setText(repo["name"]) # set the now building label
 
+        # data
+        self.model_pack_folder = ""
+        self.texture_pack_folder = ""
+
     # Slots
     def pick_texture_pack_folder(self):
-        print("Recieved Texture Pack select!")
-    
+        # create a file picker object
+        picker = QtWidgets.QFileDialog(self)
+
+        # configure and run
+        picker.setFileMode(QtWidgets.QFileDialog.FileMode.DirectoryOnly)
+        picker.exec()
+
+        # set the lineEdit content to the picker result
+        self.texture_pack_folder = picker.selectedFiles()[0]
+        self.ui.texture_pack_folder.setText(picker.selectedFiles()[0])
+
     def pick_model_pack_folder(self):
-        print("Recieved Model Pack Select!")
+        # create a file picker object
+        picker = QtWidgets.QFileDialog(self)
+
+        # configure and exec
+        picker.setFileMode(QtWidgets.QFileDialog.FileMode.DirectoryOnly)
+        picker.exec()
+
+        # set the lineEdit content to the chosen folder
+        self.model_pack_folder = picker.selectedFiles()[0]
+        self.ui.model_pack_folder.setText(picker.selectedFiles()[0])
     
     def b_continue(self):
-        print("Recieved Continue!")
+        dialog = BaseromSelectDialog(parent=None)
+        dialog.exec()
